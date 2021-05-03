2c509d03de93709270317647690e76911129c5d4
opera7133 <ainznino@pm.me>
2021-05-03 18:26:04 +0900

    画面遷移 + ページ + AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -3,39 +3,88 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:stilo/stilo.dart';
+import './single.dart';
+import './multi.dart';
 
 void main() => runApp(MyApp());
-
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    final wordPair1 =WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter First App'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+      title: "Let's get started",
+      home: Start(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords>{
+class Start extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('LightCube'),
+          leading: addLeadingIcon(),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Container(
+                width: StiloWidth.w48,
+                height: StiloHeight.h12,
+                child: ElevatedButton(
+                  child: const Text('Single'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.lightBlue,
+                    onPrimary: Colors.white,
+                  ),
+                  onPressed: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Single()),
+                    );
+                  },
+              )),
+              StiloSpacing.y10,
+              Container(
+                width: StiloWidth.w48,
+                height: StiloHeight.h12,
+                  child: ElevatedButton(
+                    child: const Text('Multi'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.lightBlue,
+                        onPrimary: Colors.white,
+                      ),
+                    onPressed: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Multi()),
+                      );
+                    },
+              )),
+            ]
+          ),
+        ),
+    );
   }
+}
+
+Widget addLeadingIcon() {
+  return new Container(
+    padding: EdgeInsets.only(left: 10),
+    height: 50.0,
+    width: 50.0,
+    child: new Stack(
+      alignment: AlignmentDirectional.center,
+      children: <Widget>[
+        new Image.asset(
+          'images/logo.png',
+          width: 50.0,
+          height: 50.0,
+        )
+      ],
+    ),
+  );
 }
